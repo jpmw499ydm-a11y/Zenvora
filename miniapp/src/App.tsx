16640b1407d6476d588e7e318af54268d2dba4a8
jpmw499ydm-a11y <jpmw499ydm@privaterelay.appleid.com>
2026-07-23 23:05:31 +0300

    Update App.tsx

--- miniapp/src/App.tsx
+++ miniapp/src/App.tsx
@@ -464,7 +464,7 @@ export default function App() {
           />
           <span
             className={
-              connectionStatus === "connected" ? "active" : ""
+              connectionStatus === "checking" ? "active" : ""
             }
           />
         </div>
